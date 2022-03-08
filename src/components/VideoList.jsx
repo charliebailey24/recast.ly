29ceb208d77f9d7963a7f055e6869c39dde5652a
Charlie <charliebailey24@gmail.com>
2022-03-07 17:41:43 -0800

    cleaned up console logs

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,8 +3,6 @@ import exampleVideoData from '/src/data/exampleVideoData.js';
 // taking piece of data and making avaliable to use
 
 var VideoList = (props) => {
-  console.log('VideoList props: ', props);
-  // console.log('exampleVideoData: ', exampleVideoData);
   // <div className="video-list">
   //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
   //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -16,6 +14,7 @@ var VideoList = (props) => {
   // iterate over props using map
   // map each videoList entry to an h5 element
   const videoEntry = props.videos;
+
   // The best way to pick a key is to use a string that uniquely identifies a list item among its siblings. Most often you would use IDs from your data as keys:
   const videoList = videoEntry.map((video) =>
     <h5 key={video.id.videoId}>{video.snippet.description}</h5>
@@ -33,29 +32,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var GroceryListItem = (props) => {
-  const items = props.items;
-  // console.log(items);
-
-  const listItems = items.map((item) =>
-    <li>{item}</li>
-  );
-
-  return (
-    <ul>{listItems}</ul>
-  );
-};
